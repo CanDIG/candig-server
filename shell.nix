f10315b1afa67b33553f0d2cb7c147bfd3c5d451
Amanjeev Sethi <aj@amanjeev.com>
2020-05-04 17:13:50 -0400

    updates (nix dev): shellHook is required for libstd

--- shell.nix
+++ shell.nix
@@ -5,5 +5,10 @@ mkShell {
     git
     python36Full
     zlib
-    ];
+    docker
+  ];
+  shellHook = ''
+    export PYTHONPATH=`pwd`/$VENV/${python.sitePackages}/:$PYTHONPATH
+    export LD_LIBRARY_PATH=${lib.makeLibraryPath [stdenv.cc.cc]}
+  '';
 }
