d56987c58fa2a594be8d730a6f41dd1a36b720f8
Jimmy Li <lihaoyuan1996@gmail.com>
2020-11-02 16:49:53 -0800

    add patientId

--- scripts/snapshot_stats.py
+++ scripts/snapshot_stats.py
@@ -16,13 +16,19 @@ globals().update(models) # Upgrade all of the tables to be global variables
 # number of total datasets
 print("The database contains {} datasets".format(dataset.select().count()))
 
+dataset_query = dataset.select()
+
 # TODO: print the number of records under each table for each dataset, maybe in the form of a table
-for e in dataset.select():
-    print(e.name)
+for d in dataset_query:
+    print(d.name)
     for table in [patient, sample, diagnosis, sample, treatment, chemotherapy, celltransplant, study, labtest, outcome, slide, consent, immunotherapy, complication, enrollment, tumourboard, surgery, radiotherapy, variantset, readgroupset]:
         print(table)
-        print(table.select().where(table.datasetId == e).count())
+        print(table.select().where(table.datasetId == d).count())
 
 # TODO: Print out a list of patient IDs for every table
+for d in dataset_query:
+    print(d.name)
+    print([patient.name for patient in patient.select().where(patient.datasetId == d)])
+
 
 # TODO: Determine the output of this, do we use txt, rst, or something else?
