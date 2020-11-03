c5f4c2104ddd0eecfc31bad6280ead80b444960e
Jimmy Li <lihaoyuan1996@gmail.com>
2020-11-03 09:51:52 -0800

    update

--- scripts/snapshot_stats.py
+++ scripts/snapshot_stats.py
@@ -4,6 +4,8 @@ from peewee import SqliteDatabase
 from playhouse.reflection import generate_models  # print_model, print_table_sql
 
 current_utc = datetime.utcnow()
+
+print("CanDIG-Server DataBase Snapshot Report")
 print("Report generated on {} UTC".format(current_utc))
 
 # TODO: make this more robust
@@ -21,19 +23,24 @@ dataset_query = dataset_model.select()
 
 clin_tables = ['patient', 'sample', 'diagnosis', 'sample', 'treatment', 'chemotherapy', 'celltransplant', 'study', 'labtest', 'outcome', 'slide', 'consent', 'immunotherapy', 'complication', 'enrollment', 'tumourboard', 'surgery', 'radiotherapy', 'variantset', 'readgroupset']
 
-# TODO: print the number of records under each table for each dataset, maybe in the form of a table
+# print the number of records under each table for each dataset, maybe in the form of a table
 for d in dataset_query:
     print(d.name)
     for table in clin_tables:
         table_model = models[table]
         print(table)
-        print(table.select().where(table.datasetId == d).count())
+        print(table_model.select().where(table_model.datasetId == d).count())
 
-# TODO: Print out a list of patient IDs for every table
+# Print out a list of patient IDs for every table
 patient_model = models['patient']
 for d in dataset_query:
     print(d.name)
     print([patient.name for patient in patient_model.select().where(patient_model.datasetId == d)])
 
+# TODO: Print out a list of peers
+peer_model = models['peer']
+print([peer.url for peer in peer_model.select()])
 
-# TODO: Determine the output of this, do we use txt, rst, or something else?
+
+# TODO: Output a report, possibly in md that allows us to print tables, we can look at txt as well.
+
